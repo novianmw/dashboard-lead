86002bdbc525bae6dbf8e445e85630c26fb91bac
novianmw <novian.mulyoadi.w@gmail.com>
2023-11-12 20:24:13 +0700

    update

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
 
 this project is about SAAS for manage your financial at once. with features :
+
 1.  Manage your Balance, Income and Outcome for monitoring your money
 2.  have Subscription monitoring
 3.  Statistic your balance, income and outcome for monitoring
--- src/app/constant/index.ts
+++ src/app/constant/index.ts
@@ -1,19 +1,183 @@
-import { RiBankLine, RiBookOpenLine, RiFunctionLine } from "react-icons/ri"
 
-export const MainMenuItems = [
+export const navLinks = [
     {
-        title: "Dashboard",
-        href: "/examples/forms",
-        logo: RiFunctionLine,
+        route: '/',
+        title: 'Home',
     },
     {
-        title: "Statistic",
-        href: "/examples/forms/account",
-        logo: RiBankLine,
+        route: '/signin',
+        title: 'Login',
+    },
+    
+]
+
+export const features = [
+    {
+        id: 'feature-1',
+        iconUrl: "/assets/star.svg",
+        title: 'Rewards',
+        content: 'The best credit cards offer some tantalizing combinations of promotions and prizes',
     },
     {
-        title: "Report",
-        href: "/examples/forms/appearance",
-        logo: RiBookOpenLine,
+        id: 'feature-2',
+        iconUrl: "/assets/Shield.svg",
+        title: '100% Secured',
+        content: 'We take proactive steps make sure your information and transactions are secure.',
+    },
+    {
+        id: 'feature-3',
+        iconUrl: "/assets/Send.svg",
+        title: 'Balance Transfer',
+        content: 'A balance transfer credit card can save you a lot of money in interest charges.',
+    },
+]
+
+export const feedback = [
+    {
+        id: 'feedback-1',
+        content: 'Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.',
+        name: 'Herman Jensen',
+        title: 'Founder & Leader',
+        imgUrl:  "/assets/poeple01.png",
+    },
+    {
+        id: 'feedback-2',
+        content: "Money makes your life easier. If you're lucky to have it, you're lucky.",
+        name: 'Steve Mark',
+        title: 'Founder & Leader',
+        imgUrl: "/assets/poeple02.png",
+    },
+    {
+        id: 'feedback-3',
+        content: 'It is usually people in the money business, finance, and international trade that are really rich.',
+        name: 'Kenn Gallagher',
+        title: 'Founder & Leader',
+        imgUrl: "/assets/poeple03.png",
+    },
+]
+
+export const stats = [
+    {
+        id: 'stats-1',
+        title: 'User Active',
+        value: '3800+',
+    },
+    {
+        id: 'stats-2',
+        title: 'Trusted by Company',
+        value: '230+',
+    },
+    {
+        id: 'stats-3',
+        title: 'Transaction',
+        value: '$230M+',
+    },
+]
+
+export const footerLinks = [
+    {
+        title: 'Useful Links',
+        links: [
+            {
+                name: 'Content',
+                link: 'https://www.hoobank.com/content/',
+            },
+            {
+                name: 'How it Works',
+                link: 'https://www.hoobank.com/how-it-works/',
+            },
+            {
+                name: 'Create',
+                link: 'https://www.hoobank.com/create/',
+            },
+            {
+                name: 'Explore',
+                link: 'https://www.hoobank.com/explore/',
+            },
+            {
+                name: 'Terms & Services',
+                link: 'https://www.hoobank.com/terms-and-services/',
+            },
+        ],
+    },
+    {
+        title: 'Community',
+        links: [
+            {
+                name: 'Help Center',
+                link: 'https://www.hoobank.com/help-center/',
+            },
+            {
+                name: 'Partners',
+                link: 'https://www.hoobank.com/partners/',
+            },
+            {
+                name: 'Suggestions',
+                link: 'https://www.hoobank.com/suggestions/',
+            },
+            {
+                name: 'Blog',
+                link: 'https://www.hoobank.com/blog/',
+            },
+            {
+                name: 'Newsletters',
+                link: 'https://www.hoobank.com/newsletters/',
+            },
+        ],
+    },
+    {
+        title: 'Partner',
+        links: [
+            {
+                name: 'Our Partner',
+                link: 'https://www.hoobank.com/our-partner/',
+            },
+            {
+                name: 'Become a Partner',
+                link: 'https://www.hoobank.com/become-a-partner/',
+            },
+        ],
+    },
+]
+
+export const socialMedia = [
+    {
+        id: 'social-media-1',
+        iconUrl: "/assets/instagram.svg",
+        link: 'https://www.instagram.com/',
+    },
+    {
+        id: 'social-media-2',
+        iconUrl: "/assets/facebook.svg",
+        link: 'https://www.facebook.com/',
+    },
+    {
+        id: 'social-media-3',
+        iconUrl: "/assets/twitter.svg",
+        link: 'https://www.twitter.com/',
+    },
+    {
+        id: 'social-media-4',
+        iconUrl: "/assets/linkedin.svg",
+        link: 'https://www.linkedin.com/',
+    },
+]
+
+export const clients = [
+    {
+        id: 'client-1',
+        logo: "/assets/airbnb.png",
+    },
+    {
+        id: 'client-2',
+        logo: "/assets/binance.png",
+    },
+    {
+        id: 'client-3',
+        logo: "/assets/coinbase.png",
+    },
+    {
+        id: 'client-4',
+        logo: "/assets/dropbox.png",
     },
 ]
