93f62fcd9611e9fd68d9550dc28f5ed44e5425c7
novianmw <novian.mulyoadi.w@gmail.com>
2023-11-13 08:15:37 +0700

    update minor

--- src/components/landingPage/Business.tsx
+++ src/components/landingPage/Business.tsx
@@ -22,7 +22,7 @@ export default function Business() {
     return (
         <section id="features" className="flex flex-col px-12 py-12">
             <div className="flex-col flex justify-center items-start">
-                <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full">You do the business. <br className="sm:block hidden"></br>we`&apos;`ll handle the money.</h2>
+                <h2 className="font-poppins font-semibold xs:text-[48px] text-[40px] xs:leading-[76.8px] leading-[66.8px] w-full">You do the business. <br className="sm:block hidden"></br>we&apos;ll handle the money.</h2>
                 <p className="font-poppins font-normal text-dimWhite text-[18px] leading-[30.8px] max-w-[470px] mt-5">With the right credit card, you can improve your financial life by building credit, earning reward and saving money. But with hundreds of credit cards on the market</p>
                 <Button/>
             </div>
